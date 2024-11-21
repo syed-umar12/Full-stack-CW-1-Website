1838b5d7669b8f73f81d2e931197243a1c0eb35c
Syed Umar <mohdumerr0@gmail.com>
2024-11-21 23:49:27 +0500

    Added lessons

---
+++ app.js
@@ -0,0 +1,18 @@
+new Vue({
+    el: '#app',
+    data: {
+        lessons: [
+            {id: 1, subject: 'Arts', location: 'Toronto', price: 50, spaces: 5},
+            {id: 2, subject: 'chess', location: 'Tokyo', price: 100, spaces: 4},
+            {id: 3, subject: 'Swimming', location: 'Sydney', price: 120, spaces: 2},
+            {id: 4, subject: 'Foreign language', location: 'Islamabad', price: 150, spaces: 1},
+            {id: 5, subject: 'Theater', location: 'Dhaka', price: 175, spaces: 3},
+            {id: 6, subject: 'Sports', location: 'California', price: 200, spaces: 5},
+            {id: 7, subject: 'Music', location:'London', price: 210, spaces: 1},
+            {id: 8, subject: 'Creative writing', location: 'Dubai', price: 250, spaces: 4},
+            {id: 9, subject: 'Science', location: 'Doha', price: 275, spaces: 2},
+            {id: 10, subject: 'Maths', location: 'Mumbai', price: 300, spaces: 3}
+
+        ]
+    }
+})
